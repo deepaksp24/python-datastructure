2b5774c17cfc11e86ca7ae2165a308aa5ca13b9c
deepak s p <96986851+deepaksp24@users.noreply.github.com>
2025-02-09 15:19:22 +0000

    just update

--- heap/heap.py
+++ heap/heap.py
@@ -1,12 +1,15 @@
+# changes added
 heap = []
 
-def insert(ele,heap):
+
+def insert(ele, heap):
     heap.append(ele)
 
     if len(heap) > 1:
-        shift_up(len(heap)-1,heap)
+        shift_up(len(heap)-1, heap)
 
     return True
+
 
 def get_max(heap):
     if not heap:
@@ -15,22 +18,24 @@ def get_max(heap):
     last_ele = heap.pop()
 
     if heap:
-        heap[0] = last_ele 
-        shift_down(0,heap)
+        heap[0] = last_ele
+        shift_down(0, heap)
     return max_ele
 
-def shift_up(index,heap):
-     parent = get_parent(index)
 
-     while parent is not None and heap[parent] < heap[index]:
-         heap[parent],heap[index] =  heap[index],heap[parent]
-         index = parent
-         parent = get_parent(index)
+def shift_up(index, heap):
+    parent = get_parent(index)
 
-def shift_down(index,heap):
+    while parent is not None and heap[parent] < heap[index]:
+        heap[parent], heap[index] = heap[index], heap[parent]
+        index = parent
+        parent = get_parent(index)
+
+
+def shift_down(index, heap):
     while True:
-        r_child = 2 * index + 1  if 2 * index + 1 < len(heap) else None
-        l_child = 2 * index + 2  if 2 * index + 2  < len(heap) else None
+        r_child = 2 * index + 1 if 2 * index + 1 < len(heap) else None
+        l_child = 2 * index + 2 if 2 * index + 2 < len(heap) else None
 
         largest = index
 
@@ -40,20 +45,19 @@ def shift_down(index,heap):
             largest = r_child
 
         if largest != index:
-            heap[index],heap[largest] = heap[largest],heap[index]
+            heap[index], heap[largest] = heap[largest], heap[index]
             index = largest
         else:
             break
-    
 
-    
 
 def get_parent(index):
-    return (index-1)//2 if index !=0 else None
+    return (index-1)//2 if index != 0 else None
 
-lst = [3,4,5,5,6,7,8,9]
+
+lst = [3, 4, 5, 5, 6, 7, 8, 9]
 for i in lst:
-    insert(i,heap)
+    insert(i, heap)
 
 
 for i in lst:
